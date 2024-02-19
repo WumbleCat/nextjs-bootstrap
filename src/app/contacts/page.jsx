4efb018fd966c52d8befba6f3daa21606dec5f87
WumbleCat <komethtauch04@gmail.com>
2024-02-19 20:31:55 +0000

    nothing

--- src/app/contacts/page.jsx
+++ src/app/contacts/page.jsx
@@ -1,43 +1,43 @@
-import React from 'react';
-import Image from 'next/image';
+// import React from 'react';
+// import Image from 'next/image';
 
-export default function Plan() {
-    const reps = [
-        { "names": "Joshua Man Yu Ng", "email": "jq22530@bristol.ac.uk", "LinkedIn": "https://www.linkedin.com/in/kometh-tauch-38a58626b/"  },
-        { "names": "Kometh Tauch (Maine)", "email": "cy23929@bristol.ac.uk", "LinkedIn": "https://www.linkedin.com/in/kometh-tauch-38a58626b/" }
-    ];
+// export default function Plan() {
+//     const reps = [
+//         { "names": "Joshua Man Yu Ng", "email": "jq22530@bristol.ac.uk", "LinkedIn": "https://www.linkedin.com/in/kometh-tauch-38a58626b/"  },
+//         { "names": "Kometh Tauch (Maine)", "email": "cy23929@bristol.ac.uk", "LinkedIn": "https://www.linkedin.com/in/kometh-tauch-38a58626b/" }
+//     ];
 
-    return (
-      <div className="container mt-5">
-        <h1 className='text-center pb-3'>Meet Our Reps!</h1>
-          <div className="row justify-content-center">
-              {reps.map((rep, index) => (
-                  <div key={index} className="col-md-4 mb-4">
-                      <div className="card p-0">
-                          <div className="card-body text-center p-2">
-                              <Image 
-                                  src="/images/UoB_building.jpg"
-                                  alt="pics"
-                                  height={150}
-                                  width={200}
-                                  className="mx-auto" // Center the image horizontally
-                              />
-                              <h2 className="card-title text-lg font-bold my-2">Student Representative</h2>
-                              <p className="card-text text-gray-700">
-                                  <span className="font-bold">Name:</span> {rep.names}
-                              </p>
-                              <p className="card-text text-gray-700">
-                                  <span className="font-bold">Email:</span> {rep.email}
-                              </p>
-                              <p className="card-text text-gray-700">
-                                  <span className="font-bold text-blue-500">LinkedIn:</span> <a href={rep.LinkedIn} target="_blank" rel="noopener noreferrer" className="text-blue-500">{rep.names}</a>
-                              </p>
-                          </div>
-                      </div>
-                  </div>
-              ))}
-          </div>
-      </div>
-  );
+//     return (
+//       <div className="container mt-5">
+//         <h1 className='text-center pb-3'>Meet Our Reps!</h1>
+//           <div className="row justify-content-center">
+//               {reps.map((rep, index) => (
+//                   <div key={index} className="col-md-4 mb-4">
+//                       <div className="card p-0">
+//                           <div className="card-body text-center p-2">
+//                               <Image 
+//                                   src="/images/UoB_building.jpg"
+//                                   alt="pics"
+//                                   height={150}
+//                                   width={200}
+//                                   className="mx-auto" // Center the image horizontally
+//                               />
+//                               <h2 className="card-title text-lg font-bold my-2">Student Representative</h2>
+//                               <p className="card-text text-gray-700">
+//                                   <span className="font-bold">Name:</span> {rep.names}
+//                               </p>
+//                               <p className="card-text text-gray-700">
+//                                   <span className="font-bold">Email:</span> {rep.email}
+//                               </p>
+//                               <p className="card-text text-gray-700">
+//                                   <span className="font-bold text-blue-500">LinkedIn:</span> <a href={rep.LinkedIn} target="_blank" rel="noopener noreferrer" className="text-blue-500">{rep.names}</a>
+//                               </p>
+//                           </div>
+//                       </div>
+//                   </div>
+//               ))}
+//           </div>
+//       </div>
+//   );
   
-}
+// }
--- src/app/links/page.jsx
+++ src/app/links/page.jsx
@@ -1,62 +1,62 @@
-import React from 'react';
+// import React from 'react';
 
-export default function Links() {
-    const links = [
-        {
-            "Title": "Calendar (deadlines)",
-            "Desc": "View deadlines for the course.",
-            "Link": "https://shorturl.at/nCKM9"
-        },
-        {
-            "Title": "Assessment Spreadsheet",
-            "Desc": "Details on how your math courses will be assessed.",
-            "Link": "https://shorturl.at/dgAG1"
-        },
-        {
-            "Title": "Google Drive",
-            "Desc": "Access course materials and self-learning materials.",
-            "Link": "https://drive.google.com/drive/u/0/folders/10Xoi_NbUD9L_H4fllO3inNZW8pdOkBkC"
-        },
-        {
-            "Title": "Anonymous Message Box",
-            "Desc": "Submit anonymous messages.",
-            "Link": "https://forms.gle/yBACcnHkHndws1Fo7"
-        },
-        {
-          "Title": "Discord Server",
-          "Desc": "Join our discord server",
-          "Link": "https://forms.gle/yBACcnHkHndws1Fo7"
-        },
-        {
-          "Title": "WhatsApp Group",
-          "Desc": "Join our WhatsApp Group",
-          "Link": "https://forms.gle/yBACcnHkHndws1Fo7"
-        },
-    ];
+// export default function Links() {
+//     const links = [
+//         {
+//             "Title": "Calendar (deadlines)",
+//             "Desc": "View deadlines for the course.",
+//             "Link": "https://shorturl.at/nCKM9"
+//         },
+//         {
+//             "Title": "Assessment Spreadsheet",
+//             "Desc": "Details on how your math courses will be assessed.",
+//             "Link": "https://shorturl.at/dgAG1"
+//         },
+//         {
+//             "Title": "Google Drive",
+//             "Desc": "Access course materials and self-learning materials.",
+//             "Link": "https://drive.google.com/drive/u/0/folders/10Xoi_NbUD9L_H4fllO3inNZW8pdOkBkC"
+//         },
+//         {
+//             "Title": "Anonymous Message Box",
+//             "Desc": "Submit anonymous messages.",
+//             "Link": "https://forms.gle/yBACcnHkHndws1Fo7"
+//         },
+//         {
+//           "Title": "Discord Server",
+//           "Desc": "Join our discord server",
+//           "Link": "https://forms.gle/yBACcnHkHndws1Fo7"
+//         },
+//         {
+//           "Title": "WhatsApp Group",
+//           "Desc": "Join our WhatsApp Group",
+//           "Link": "https://forms.gle/yBACcnHkHndws1Fo7"
+//         },
+//     ];
 
-    // Sort links array alphabetically by Title
-    links.sort((a, b) => a.Title.localeCompare(b.Title));
+//     // Sort links array alphabetically by Title
+//     links.sort((a, b) => a.Title.localeCompare(b.Title));
 
-    return (
-        <div className="container mx-auto px-4 py-8">
-            <h2 className="text-2xl font-bold m-4">Useful Documents</h2>
-            <div className="accordion" id="accordionExample">
-                {links.map((doc, index) => (
-                    <div key={index} className="accordion-item">
-                        <h2 className="accordion-header" id={`heading${index}`}>
-                            <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target={`#collapse${index}`} aria-expanded="true" aria-controls={`collapse${index}`}>
-                                {doc.Title}
-                            </button>
-                        </h2>
-                        <div id={`collapse${index}`} className="accordion-collapse collapse" aria-labelledby={`heading${index}`} data-bs-parent="#accordionExample">
-                            <div className="accordion-body">
-                                <p>{doc.Desc}</p>
-                                <a href={doc.Link} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">{doc.Link}</a>
-                            </div>
-                        </div>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
-}
+//     return (
+//         <div className="container mx-auto px-4 py-8">
+//             <h2 className="text-2xl font-bold m-4">Useful Documents</h2>
+//             <div className="accordion" id="accordionExample">
+//                 {links.map((doc, index) => (
+//                     <div key={index} className="accordion-item">
+//                         <h2 className="accordion-header" id={`heading${index}`}>
+//                             <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target={`#collapse${index}`} aria-expanded="true" aria-controls={`collapse${index}`}>
+//                                 {doc.Title}
+//                             </button>
+//                         </h2>
+//                         <div id={`collapse${index}`} className="accordion-collapse collapse" aria-labelledby={`heading${index}`} data-bs-parent="#accordionExample">
+//                             <div className="accordion-body">
+//                                 <p>{doc.Desc}</p>
+//                                 <a href={doc.Link} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">{doc.Link}</a>
+//                             </div>
+//                         </div>
+//                     </div>
+//                 ))}
+//             </div>
+//         </div>
+//     );
+// }
