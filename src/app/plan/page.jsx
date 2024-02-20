1cc2f86a6276395af2b131266b11579e2dd82a44
WumbleCat <komethtauch04@gmail.com>
2024-02-20 15:47:53 +0000

    comment plan

--- src/app/plan/page.jsx
+++ src/app/plan/page.jsx
@@ -1,47 +1,50 @@
-// import React from 'react';
+import React from 'react';
 
-// export default function PlanAccordion() {
-//     return (
-//         <div className="accordion" id="accordionExample">
-//             {/* Section 1 */}
-//             <div className="accordion-item">
-//                 <h2 className="accordion-header" id="headingOne">
-//                     <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-//                         Year 1 Course Representatives- Vision and Plan
-//                     </button>
-//                 </h2>
-//                 <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-//                     <div className="accordion-body">
-//                         <p><strong>(Modified in Feb 2024)</strong></p>
-//                         <ol>
-//                             <li>
-//                                 <strong>Regularly collect opinions:</strong> Collecting opinions regularly through channels such as WhatsApp, Discord, and an anonymous message box. Attending Year 1 SSLC and full SSLC meetings to reflect opinions on course arrangements and school policies.
-//                             </li>
-//                             <li>
-//                                 <strong>Provide assistance on academics:</strong> Develop an online learning platform, create a roadmap project, hold voluntary math and coding sessions, and assist in forming study groups.
-//                             </li>
-//                             <li>
-//                                 <strong>Provide suggestions on school policies:</strong> Send inquiry letters to request the school to clarify its policies, focusing on various school policies that may affect students learning environment.
-//                             </li>
-//                             <li>
-//                                 <strong>Manage learning differences and allow capable students to develop:</strong> Provide updates on available scholarships and encourage students to host activities.
-//                             </li>
-//                             <li>
-//                                 <strong>Enhance programme competitiveness through curriculum reform:</strong> Collaborate with school authority to express concerns on possible modification of course curriculum.
-//                             </li>
-//                             <li>
-//                                 <strong>Enhance career development:</strong> Promote career opportunities, evaluate the current placement year arrangements, lobby for more internship opportunities, and collaborate with career service teams.
-//                             </li>
-//                             <li>
-//                                 <strong>Develop a strong Data Science community:</strong> Maintain and expand the Discord community, create branches corresponding to each academic year, and collaborate with BDSS/Study Skills Team.
-//                             </li>
-//                             <li>
-//                                 <strong>Collaborate with school authorities:</strong> Explore opportunities to promote the Data Science Programme and the community, and conduct in-depth research on special topics.
-//                             </li>
-//                         </ol>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// }
+export default function PlanAccordion() {
+    return (
+        // <div className="accordion" id="accordionExample">
+        //     {/* Section 1 */}
+        //     <div className="accordion-item">
+        //         <h2 className="accordion-header" id="headingOne">
+        //             <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        //                 Year 1 Course Representatives- Vision and Plan
+        //             </button>
+        //         </h2>
+        //         <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+        //             <div className="accordion-body">
+        //                 <p><strong>(Modified in Feb 2024)</strong></p>
+        //                 <ol>
+        //                     <li>
+        //                         <strong>Regularly collect opinions:</strong> Collecting opinions regularly through channels such as WhatsApp, Discord, and an anonymous message box. Attending Year 1 SSLC and full SSLC meetings to reflect opinions on course arrangements and school policies.
+        //                     </li>
+        //                     <li>
+        //                         <strong>Provide assistance on academics:</strong> Develop an online learning platform, create a roadmap project, hold voluntary math and coding sessions, and assist in forming study groups.
+        //                     </li>
+        //                     <li>
+        //                         <strong>Provide suggestions on school policies:</strong> Send inquiry letters to request the school to clarify its policies, focusing on various school policies that may affect students learning environment.
+        //                     </li>
+        //                     <li>
+        //                         <strong>Manage learning differences and allow capable students to develop:</strong> Provide updates on available scholarships and encourage students to host activities.
+        //                     </li>
+        //                     <li>
+        //                         <strong>Enhance programme competitiveness through curriculum reform:</strong> Collaborate with school authority to express concerns on possible modification of course curriculum.
+        //                     </li>
+        //                     <li>
+        //                         <strong>Enhance career development:</strong> Promote career opportunities, evaluate the current placement year arrangements, lobby for more internship opportunities, and collaborate with career service teams.
+        //                     </li>
+        //                     <li>
+        //                         <strong>Develop a strong Data Science community:</strong> Maintain and expand the Discord community, create branches corresponding to each academic year, and collaborate with BDSS/Study Skills Team.
+        //                     </li>
+        //                     <li>
+        //                         <strong>Collaborate with school authorities:</strong> Explore opportunities to promote the Data Science Programme and the community, and conduct in-depth research on special topics.
+        //                     </li>
+        //                 </ol>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
+        <div>
+          yo
+        </div>
+    );
+}
